f08278b4f34ae995da33f4d1f4740ef395218002
Zach Mostowsky <zachary.mostowsky@ntconcepts.com>
2018-10-22 17:59:52 -0400

    Initial Commit

---
+++ app/src/main/java/com/geog686/geog686finalproject/MainActivity.java
@@ -0,0 +1,13 @@
+package com.geog686.geog686finalproject;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
